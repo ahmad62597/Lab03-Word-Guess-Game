0639818bccfc16971c634c9620bbaf00267b5f4b
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-15 11:36:20 -0800

    check options function

--- Lab03-GuessingGame/Lab03-GuessingGame/Program.cs
+++ Lab03-GuessingGame/Lab03-GuessingGame/Program.cs
@@ -11,12 +11,41 @@ namespace Lab03_GuessingGame
         }
 
         static void MainMenu()
-        {
+        {   
+            //create user menu
             Console.WriteLine("Hello, welcome to Console Hangman.");
             Console.WriteLine("");
             Console.WriteLine("Press 1 to play.");
             Console.WriteLine("Press 2 to exit.");
             Console.WriteLine("Press 3 to go to settings.");
+            
+            //check user input
+            string userInput = Console.ReadLine();
+            ReadUserOption(userInput);
         }
+
+        static void ReadUserOption (string userInput)
+
+        {   //read users input from main menu and direct them to appropriate section
+            
+            if (userInput == "1")
+            {
+                PlayGame();
+            }
+
+            if (userInput == "2")
+            {
+                EndGame();
+            }
+
+            if (userInput == "3")
+            {
+                Setting();
+            }
+
+
+        }
+
+
     }
 }

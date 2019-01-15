12ababfaa48c2292230782d80e355eed233587fc
ahmad <ahmad@DESKTOP-7Q4GTCL>
2019-01-15 11:24:17 -0800

    Main Menu

---
+++ Lab03-GuessingGame/Lab03-GuessingGame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab03_GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Hello, welcome to Console Hangman.");
+            Console.WriteLine("");
+            Console.WriteLine("Press 1 to play.");
+            Console.WriteLine("Press 2 to exit.");
+            Console.WriteLine("Press 3 to go to settings.");
+        }
+    }
+}
